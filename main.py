f814e4b62a85321d24f2efe214896c2007278ad8
WilawanLelapratak <wilawan.wove@gmail.com>
2017-01-03 05:07:38 +0700

    Final Game II : Show Eevee

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ class EeveeGameWindow(arcade.Window) :
     def __init__(self, width, height) :
         super().__init__(width, height)
         self.image = arcade.load_texture('desertbg.png')
+        self.eevee = arcade.Sprite('eevee.png')
+        self.eevee.set_position(100, 100)
 
     def on_draw(self) :
         arcade.start_render()
         arcade.draw_texture_rectangle(300, 300, SCREEN_WIDTH, SCREEN_HEIGHT,self.image)
+        self.eevee.draw()
 
 if __name__ == '__main__' :
     window = EeveeGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
