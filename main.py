bb8d8f50882cc6847e6f760f2a365afdf4778877
WilawanLelapratak <wilawan.wove@gmail.com>
2017-01-03 05:16:40 +0700

    Final Game III : Eevee Can Run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import arcade
+from models import Eevee
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -7,13 +8,19 @@ class EeveeGameWindow(arcade.Window) :
     def __init__(self, width, height) :
         super().__init__(width, height)
         self.image = arcade.load_texture('desertbg.png')
-        self.eevee = arcade.Sprite('eevee.png')
-        self.eevee.set_position(100, 100)
+        self.eevee = Eevee(100, 100)
+        self.eevee_sprite = arcade.Sprite('eevee.png')
 
     def on_draw(self) :
         arcade.start_render()
         arcade.draw_texture_rectangle(300, 300, SCREEN_WIDTH, SCREEN_HEIGHT,self.image)
-        self.eevee.draw()
+        self.eevee_sprite.draw()
+
+    def animate(self, delta) :
+        eevee = self.eevee
+
+        eevee.animate(delta)
+        self.eevee_sprite.set_position(eevee.x, eevee.y)
 
 if __name__ == '__main__' :
     window = EeveeGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,2 +1,12 @@
 import arcade.key
 from random import randint, random
+
+class Eevee :
+    def __init__(self, x, y) :
+        self.x = x
+        self.y = y
+
+    def animate(self, delta) :
+        if self.y > 600 :
+            self.y = 0
+        self.y += 5
