90766676cbd80481a9d2428cbfc0f108ebfab076
WilawanLelapratak <wilawan.wove@gmail.com>
2017-01-03 06:32:03 +0700

    Final Game V : Show Candy

--- main.py
+++ main.py
@@ -4,21 +4,38 @@ from models import Eevee,World
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
+class ModelSprite(arcade.Sprite) :
+    def __init__(self, *args, **kwargs) :
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self) :
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self) :
+        self.sync_with_model()
+        super().draw()
+
 class EeveeGameWindow(arcade.Window) :
     def __init__(self, width, height) :
         super().__init__(width, height)
         self.image = arcade.load_texture('desertbg.png')
-        self.eevee_sprite = arcade.Sprite('eevee.png')
         self.world = World(width, height)
+        self.eevee_sprite = ModelSprite('eevee.png', model = self.world.eevee)
+        self.candy_sprite = ModelSprite('Caandyy.png', model = self.world.candy)
 
     def on_draw(self) :
         arcade.start_render()
         arcade.draw_texture_rectangle(300, 300, SCREEN_WIDTH, SCREEN_HEIGHT,self.image)
         self.eevee_sprite.draw()
+        self.candy_sprite.draw()
 
     def animate(self, delta) :
         self.world.animate(delta)
-        self.eevee_sprite.set_position(self.world.eevee.x, self.world.eevee.y)
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 if __name__ == '__main__' :
     window = EeveeGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -2,21 +2,47 @@ import arcade.key
 from random import randint, random
 
 class Eevee :
+    DIR_RIGHT = 0
+    DIR_UP = 1
+
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.direction = Eevee.DIR_RIGHT
+
+    def switch_direction(self):
+        if self.direction == Eevee.DIR_RIGHT:
+            self.direction = Eevee.DIR_UP
+        else:
+            self.direction = Eevee.DIR_RIGHT
+
+    def animate(self, delta):
+        if self.direction == Eevee.DIR_UP:
+            if self.y > self.world.height:
+                self.y = 0
+            self.y += 5
+        else:
+            if self.x > self.world.width:
+                self.x = 0
+            self.x += 5
+
+class Candy :
     def __init__(self, world, x, y) :
         self.world = world
         self.x = x
         self.y = y
-
-    def animate(self, delta) :
-        if self.y > self.world.height :
-            self.y = 0
-        self.y += 5
 
 class World :
     def __init__(self, width, height) :
         self.width = width
         self.height = height
         self.eevee = Eevee(self, 100, 100)
+        self.candy = Candy(self, 400, 400)
 
     def animate(self, delta) :
         self.eevee.animate(delta)
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.SPACE:
+            self.eevee.switch_direction()
